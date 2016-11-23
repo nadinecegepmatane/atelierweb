a93a20aec54b50d6df87906a1389693a914cd750
nadinecegepmatane <nadineducegep@gmail.com>
2016-11-23 12:31:07 -0500

    liste lieux

---
+++ lanparty/lieux.php
@@ -0,0 +1,91 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=lanparty';
+$user = 'root';
+$password = '';
+
+try {
+    $basededonnees = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+$sql = 'SELECT * FROM lieu';
+$listeLieux = $basededonnees->query($sql);
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr" dir="ltr">
+<head>
+<meta charset="UTF-8" />
+<title>Lan Party Matane</title>
+<script></script>
+<link rel="stylesheet" href="normalize.css" />
+<style></style>
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+<meta content='width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui' name='viewport'>
+<link rel="stylesheet" media="all" href="style.css" />
+<script>
+</script>
+<style>
+
+	#liste-lieux li
+	{
+		color:white;
+		font-size:30px;
+		font-weight:bold;
+	}
+
+
+</style>
+</head>
+<body>
+<header class="entete-public">
+	<ul class="menu">
+		<li class="item texte jeux">
+			<a href="#">Les jeux</a> 
+		</li>
+		<li class="item texte autres-jeux">
+			<a href="#">Les "autres" jeux</a>
+		</li>
+		<li class="item texte bouffe">
+			<a href="#">La bouffe</a>
+		</li>
+		<li class="item important">
+			<img class="logo" src="image/steam2-nouveau.svg" alt="Steam de Matane"/>
+		</li>
+		<li class="item texte dates">
+			<a href="#">Les dates  </a>
+		</li>
+		<li class="item texte lieux">
+			<a href="#">Les lieux</a>
+		</li>
+		<li class="item texte contact">
+			<a href="#">Contact</a>
+		</li>
+	</ul>
+	<h1>
+		Choisissez votre Lan party
+	</h1>
+</header>
+
+
+
+<ul id="liste-lieux">
+<?php
+    foreach ($listeLieux as $lieu) 
+	{
+		?>
+		<li><?php echo $lieu['nom'];?></li>
+		<?php
+        //print $lieu['lien'] . "\t";
+    }
+
+?>
+</ul>
+
+</ul>
+</body>
+</html>
